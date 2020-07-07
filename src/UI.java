b111a1f2ce78c9d7f723e6f52d576edc84399f21
rpcluff <rpcluff@asu.edu>
2020-07-06 20:00:19 -0400

    Removed a lot to make the project more simple

--- src/UI.java
+++ src/UI.java
@@ -21,11 +21,7 @@ public class UI {
 		}
 		
 		UI ui = new UI(args[0], args[1], args[2], args[3]);
-		if (args[4].equals("e")) {
-			ui.employee();
-		} else {
-			ui.customer();
-		}
+		ui.start();
 	}
 	
 	/**
@@ -42,52 +38,23 @@ public class UI {
 		this.passwd = passwd;
 		this.driver = driver;
 	}
-	
-	/**
-	 * Starts the UI for employees
-	 */
-	private void employee() {
 		
-	}
-	
 	/**
-	 * Starts the UI for customers
+	 * Starts the UI
 	 */
-	private void customer() {
+	private void start() {
+		System.out.println("Query options: ")
 		Scanner in = new Scanner(System.in);
-		String input = "";
+		System.out.print("Enter 1 to place new order or 2 to view current order (q to quit): ");
+		String input = in.nextLine();
 		while (!input.equals("q")) {
+			
+			
 			System.out.print("Enter 1 to place new order or 2 to view current order (q to quit): ");
 			input = in.nextLine();
-			if (input.equals("1")) {
-				System.out.print("Enter email: ");
-				String email = in.nextLine();
-				if (!customerExists(email)) {
-					System.out.print("Email not found. Enter name: ");
-					String name = in.nextLine();
-					System.out.print("Enter address: ");
-					String address = in.nextLine();
-					System.out.print("Enter Phone Number: ");
-					String phone = in.nextLine();
-					addCustomer(email, name, address, phone);
-				}
-				Order order = new Order(email);
-				String orderItem = "";
-				while (!orderItem.equals("q")) {
-					
-				}
-				
-				
-			} else if (input.equals("2")) {
-				System.out.print("Enter the email on the order(s): ");
-				String email = in.nextLine();
-				displayOrders(email);
-			}
 		}
 		in.close();
 	}
-	
-	// Start Customer queries
 	
 	/**
 	 * Displays the orders of the specified customer
@@ -177,103 +144,6 @@ public class UI {
 		
 		return name;
 	}
-	
-	/**
-	 * Adds the customer to the database (Check if the customer exists beforehand)
-	 * 
-	 * Customer information:
-	 * @param email
-	 * @param name
-	 * @param address
-	 * @param phone
-	 */
-	private void addCustomer(String email, String name, String address, String phone) {
-		
-	}
-	
-	private void placeOrder() {
-		ResultSet rs = null;
-		PreparedStatement stmt = null;
-		Connection conn = null;
-		
-		try {
-			// Step 1: Load the JDBC driver
-			Class.forName(driver);
-
-			// Step 2: make a connection
-			conn = DriverManager.getConnection(url, user, passwd);
-
-			// Step 3: Create a statement
-			stmt = conn.prepareStatement(statement);
-
-			// Step 4: Make a query
-			rs = stmt.executeQuery();
-			
-			// Step 5: Display
-		}
-		catch (Exception exc)
-		{
-			exc.printStackTrace();
-		}
-		finally {  // ALWAYS clean up your DB resources
-			close(rs, stmt, conn);
-		}
-	}
-	
-	/**
-	 * Checks if a customer with the supplied email exists
-	 * @param email email of the customer
-	 * @return true if the customer exists in the database, false otherwise.
-	 */
-	private boolean customerExists(String email) {
-		boolean result = false;
-		ResultSet rs = null;
-		PreparedStatement stmt = null;
-		Connection conn = null;
-		
-		try {
-			// Step 1: Load the JDBC driver
-			Class.forName(driver);
-
-			// Step 2: make a connection
-			conn = DriverManager.getConnection(url, user, passwd);
-
-			// Step 3: Create a statement
-			stmt = conn.prepareStatement("SELECT * "
-					+ "FROM CUSTOMER as C "
-					+ "WHERE C.Email=?");
-			stmt.setString(1,  email);
-
-			// Step 4: Make a query
-			rs = stmt.executeQuery();
-			
-			// This should return true if the customer already exists in the database,
-			// or false if not
-			result = !rs.next();
-		}
-		catch (Exception exc)
-		{
-			exc.printStackTrace();
-		}
-		finally {  // ALWAYS clean up your DB resources
-			close(rs, stmt, conn);
-		}
-		
-		return result;
-	}
-	// End customer queries
-	
-	// Start Employee queries
-	
-	private void fulfillOrder() {
-		
-	}
-	
-	private void viewWarehouseOrders() {
-		
-	}
-	
-	//End Employee queries
 	
 	/**
 	 * Closes the DB resources
